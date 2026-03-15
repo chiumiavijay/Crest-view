39bef1d9a51b1650f3f8e3ac13b41fccbd4cddb8
chiumiavijay <chiumiavijay75@gmail.com>
2026-03-15 19:01:57 +0200

    Refactor PublicLayout component structure and styles
    Refactor PublicLayout to use a main wrapper and improve layout styles.

--- client/src/PublicLayout.js
+++ client/src/PublicLayout.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const PublicLayout = () => {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      
+      {/* Navbar */}
       <Navbar />
-      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <div style={{ flex: '1' }}>
-          <Outlet />
-        </div>
-        <Footer />
-      </div>
-    </>
+
+      {/* Page Content */}
+      <main style={{ flex: 1, width: "100%", maxWidth: "1200px", margin: "0 auto", padding: "10px" }}>
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+
+    </div>
   );
 };
 
